850881fb6ca7468500fd3480a44d182507a4f0c4
Paulo Santos Abreu <abreups@gmail.com>
2018-05-03 20:22:24 -0300

    commit inicial

---
+++ escreve_no_display_1/escreve_no_display_1.ino
@@ -0,0 +1,39 @@
+/*
+  Exemplo de como escrever no display OLED do ESP32 com LoRa.
+
+  Para que o display funcione, é necessário que o GPIO16 
+  vá para 0 por pelo menos 5 ms e depois fique em 1.
+  
+*/
+
+//biblioteca para comunicação com o display
+#include "SSD1306.h"
+
+// construtor do objeto do display
+// parametros: address,SDA,SCL
+// (Documentação da placa WiFi LoRa 32)
+SSD1306 display(0x3c, 4, 15); 
+
+void setup() {
+ //configura GPIO16 como saida
+  pinMode(16,OUTPUT);       // GPIO16 como saída
+  digitalWrite(16, LOW);    // reseta o OLED
+  delay(10);                // aguarda pelo menos 5 ms
+  digitalWrite(16, HIGH);   // mantém GPIO16 em 1 durante uso do OLED
+  display.init();           // inicializa o display
+  display.clear();          // limpa a tela do display
+  
+  display.drawString(0, 0, "OLED display"); // prepara texto para o display
+  display.drawString(0, 10, "Linha 2");     // prepara texto para o display
+  display.drawString(10, 20, "deslocado");  // prepara texto para o display
+  display.display();                        // mostra o texto no display
+
+  delay(5000);        // aguarda 5 s
+  display.clear();    // apaga texto que vai para o display
+  display.display();  // mostra o texto (vazio) no display
+}
+
+void loop() {
+}
+
+
